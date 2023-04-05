f5afea1e5a5b0c0cc64aea99d88802b077d50bf4
tkyg <80114433+tkyg@users.noreply.github.com>
2023-04-05 17:18:00 -0400

    creates signup and routes for user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,34 @@
 class UsersController < ApplicationController
 
+  skip_before_action :authenticate_user, only: [:index, :show, :create]
+  
+  def index
+    render json: User.all
+  end
+
+  def show
+    user = User.find_by(id: session[:user_id])
+    if user
+      render json: user
+    else
+      render json: { error: "Not Authorized" }, status: :unauthorized
+    end
+  end
+
+  def create
+    user = User.new(user_params)
+    if user.save
+      session[:user_id] = user.id
+      render json: user, status: :created
+    else
+      render json: { errors: user.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def user_params
+    params.permit(:username, :password, :role)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,10 @@ Rails.application.routes.draw do
   post "/login", to: 'sessions#create'
   delete "/logout", to: 'sessions#destroy'
 
+  post "/signup", to: "users#create"
+  get "/me", to: "users#show"
+  get "/users", to: "users#index"
+
 
   # Routing logic: fallback requests for React Router.
   # Leave this here to help deploy your app later!
